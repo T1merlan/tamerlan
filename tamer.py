c3f0a21e47321ae55cb23627b1586264ff0e4327
T1merlan <148463260+T1merlan@users.noreply.github.com>
2023-10-19 19:45:56 +0600

    Add files via upload

---
+++ tamer.py
@@ -0,0 +1,7 @@
+num1 = float(input("первое число: "))
+num2 = float(input('второе число: '))
+num3 = float(input("третье число: "))
+
+average = (num1 + num2 + num3) / 3
+
+print("Среднее значение:", average)
